b096a56fbfd20fc4d0e2ffb456e9092be327a611
TomioDeCode <tomiodecode@gmail.com>
2025-01-29 00:37:33 +0700

    chore(repo): mono => single

--- src/modules/auth/dto/register.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
-import { OmitType } from '@nestjs/mapped-types';
-
-export class RegisterDto extends OmitType(CreateUserDto, ['role'] as const) {}
